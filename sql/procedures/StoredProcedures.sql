0ea2212179c284d2433a2b3bf3c961ad0558ec00
julioferrazz <julioanjos71@gmail.com>
2025-10-14 12:20:23 -0300

    Alteração no pom e criação de triggers

--- sql/procedures/StoredProcedures.sql
+++ sql/procedures/StoredProcedures.sql
@@ -1,2 +1,38 @@
 USE db_av2
 GO
+
+Create trigger t_modificaUltimaHist on tb_historico_curiosidade
+For INSERT
+AS
+BEGIN
+	DECLARE @qtdCuriosidades INT, @maiorID int
+	Declare @id bigint, @datahora date, @curiosidadeID bigint
+
+	SELECT @qtdCuriosidades =  COUNT(*) from tb_historico_curiosidade
+	IF (@qtdCuriosidades > 12)
+	BEGIN
+		SELECT @maiorID = MAX(id) from tb_historico_curiosidade
+		Select @id = id, @datahora = data_hora_exibicao, @curiosidadeID = curiosidade_id from inserted
+
+		UPDATE tb_historico_curiosidade
+		SET id = @id, data_hora_exibicao = @datahora, curiosidade_id = @curiosidadeID
+		where id = @maiorID
+	END
+END
+GO
+
+Create trigger t_updtdelcuriosidade on tb_curiosidades
+INSTEAD OF UPDATE, DELETE
+AS
+BEGIN
+	RAISERROR('As curiosidades nao devem ser modificadas ou apagadas!', 16, 1)
+END
+GO
+
+Create trigger t_updtdelcandidato on tb_candidatos
+INSTEAD OF UPDATE, DELETE
+AS
+BEGIN
+	RAISERROR('Os candidatos nao devem ser modificados ou apagados!', 16, 1)
+END
+GO
